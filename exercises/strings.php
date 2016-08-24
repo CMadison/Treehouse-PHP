fa9ab98f18b012a2503c7981b8e24495eb88f8f6
Chris Madison <CKMadison4@gmail.com>
2016-08-24 16:26:57 -0700

    Comparison operators function similar to Javascript.

--- exercises/strings.php
+++ exercises/strings.php
@@ -1,7 +1,15 @@
 <?php
   $name = 'Chris';
   $stringOne = "Learning to display \"hello $name!\" to the screen.";
-  $stringOne = 'Learning to display "hello ' . $name . '!" to the screen.';
+  $stringOne = 'I am Learning to display "hello ' . $name . '!" to the screen.';
   echo $stringOne;
 
+  // var_dump(1 + "2");
+
+  // $a = 10;
+  // $b = '10';
+
+  // var_dump($a == $b);
+  // var_dump($a === $b);
+  var_dump($stringOne == 'Learning to display "hello Chris!" to the screen.');
 ?>
