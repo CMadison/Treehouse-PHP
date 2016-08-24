1c92165657be8af1fb7916e0f519305dc48b31cf
Chris Madison <CKMadison4@gmail.com>
2016-08-24 12:53:29 -0700

    Added kilometers to miles conversion and celsius to kelvin conversion.

---
+++ exercises/strings.php
@@ -0,0 +1,5 @@
+<?php
+  $stringOne = 'Hello World';
+  echo $stringOne;
+
+?>
--- unit_converter/units.php
+++ unit_converter/units.php
@@ -31,3 +31,41 @@ echo $kilometers;
 echo " km";
 
 ?>
+
+<?php
+  // Number of kilometes we want to convert to miles.
+  
+  // Kilometers input
+  $kilometers = 2.5;
+  // Km to miles conversion rate
+  $km_to_miles = 1.60934;
+  // Conversion logic
+  $miles = $kilometers / $km_to_miles;
+
+  echo "Distance ";
+  echo $kilometers;
+  echo " Kilometers = ";
+  echo $miles;
+  echo " miles";
+
+?>
+
+<?php
+  // Convert Celsius to Kelvin
+  
+  // Celsius input
+  $celsius = 32;
+
+  // Kelvin to celsius conversion rate
+  $celsius_to_kelvin = 273.15;
+
+  // Conversion logic
+  $kelvin = $celsius + $celsius_to_kelvin;
+
+  echo "Temperature ";
+  echo $celsius . ' degrees';
+  echo " Celsius = ";
+  echo $kelvin;
+  echo " degrees kelvin";
+
+?>
